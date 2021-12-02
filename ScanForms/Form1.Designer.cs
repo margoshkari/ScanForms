a8556356111d3ba1771a7dab7868cdcc30705f7e
babek <student@24-03>
2021-12-02 14:11:50 +0200

    Добавьте файлы проекта.

---
+++ ScanForms/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+using System.Windows.Forms;
+
+namespace ScanForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pathBtn = new System.Windows.Forms.Button();
+            this.pathTB = new System.Windows.Forms.TextBox();
+            this.nameTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // pathBtn
+            // 
+            this.pathBtn.Location = new System.Drawing.Point(262, 31);
+            this.pathBtn.Name = "pathBtn";
+            this.pathBtn.Size = new System.Drawing.Size(75, 23);
+            this.pathBtn.TabIndex = 0;
+            this.pathBtn.Text = "🔎";
+            this.pathBtn.UseVisualStyleBackColor = true;
+            this.pathBtn.Click += new System.EventHandler(this.pathBtn_Click);
+            // 
+            // pathTB
+            // 
+            this.pathTB.Location = new System.Drawing.Point(12, 33);
+            this.pathTB.Name = "pathTB";
+            this.pathTB.ReadOnly = true;
+            this.pathTB.Size = new System.Drawing.Size(244, 20);
+            this.pathTB.TabIndex = 1;
+            // 
+            // nameTB
+            // 
+            this.nameTB.Location = new System.Drawing.Point(12, 76);
+            this.nameTB.Name = "nameTB";
+            this.nameTB.Size = new System.Drawing.Size(244, 20);
+            this.nameTB.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Where";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "What";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "🔎";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 118);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(198, 316);
+            this.listBox1.TabIndex = 3;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "ext",
+            "name",
+            "creation time",
+            "last write time",
+            "last open time"});
+            this.checkedListBox1.Location = new System.Drawing.Point(217, 118);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 319);
+            this.checkedListBox1.TabIndex = 4;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            this.checkedListBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.checkedListBox1_MouseDoubleClick);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipText = "Text";
+            this.notifyIcon.BalloonTipTitle = "Ballon title";
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            this.notifyIcon.ContextMenuStrip = new ContextMenuStrip();
+            this.notifyIcon.ContextMenuStrip.Items.Add("Exit", null, this.MenuExit_Click);
+            this.notifyIcon.ContextMenuStrip.Items.Add("Show", null, this.MenuShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 450);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTB);
+            this.Controls.Add(this.pathTB);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pathBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pathBtn;
+        private System.Windows.Forms.TextBox pathTB;
+        private System.Windows.Forms.TextBox nameTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+    }
+}
+
---
+++ ScanForms/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScanForms
+{
+    public partial class Form1 : Form
+    {
+        List<string> searchPattern = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pathBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFile = new OpenFileDialog())
+            {
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+                    this.pathTB.Text = Path.GetDirectoryName(openFile.FileName);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Search();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            searchPattern.Clear();
+            for (int i = 0; i < this.checkedListBox1.CheckedItems.Count; i++)
+            {
+                searchPattern.Add(this.checkedListBox1.CheckedItems[i].ToString());
+            }
+        }
+        private void Search()
+        {
+            string name = string.Empty;
+            if (this.pathTB.Text != string.Empty)
+            {
+                this.listBox1.Items.Clear();
+                foreach (var item in Directory.GetFiles(this.pathTB.Text))
+                {
+                    if (searchPattern.Any(x => x.Contains("name")))
+                    {
+                        name = item.Split('.')[0];
+                        if (Path.GetFileName(item.Split('.')[0]).ToLower().Contains(this.nameTB.Text.ToLower()))
+                        {
+                            this.listBox1.Items.Add(Path.GetFileName(item));
+                        }
+                    }
+                    if (searchPattern.Any(x => x.Contains("creation time")))
+                    {
+                        if (File.GetCreationTime(item).ToString().Contains(this.nameTB.Text))
+                        {
+                            this.listBox1.Items.Add(Path.GetFileName(item));
+                        }
+                    }
+                    if (searchPattern.Any(x => x.Contains("ext")))
+                    {
+                        if (Path.GetFileName(item.Split('.')[1]).ToLower().Contains(this.nameTB.Text.ToLower()))
+                        {
+                            this.listBox1.Items.Add(Path.GetFileName(item));
+                        }
+                    }
+                    if (searchPattern.Any(x => x.Contains("last write time")))
+                    {
+                        if (File.GetLastWriteTime(item).ToString().Contains(this.nameTB.Text))
+                        {
+                            this.listBox1.Items.Add(Path.GetFileName(item));
+                        }
+                    }
+                    if (searchPattern.Any(x => x.Contains("last open time")))
+                    {
+                        if (File.GetLastAccessTime(item).ToString().Contains(this.nameTB.Text))
+                        {
+                            this.listBox1.Items.Add(Path.GetFileName(item));
+                        }
+                    }
+                }
+                if (this.listBox1.Items.Count == 0)
+                {
+                    MessageBox.Show("Not found!");
+                }
+                GC.Collect();
+            }
+        }
+
+        private void checkedListBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            searchPattern.Clear();
+            for (int i = 0; i < this.checkedListBox1.CheckedItems.Count; i++)
+            {
+                searchPattern.Add(this.checkedListBox1.CheckedItems[i].ToString());
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                this.notifyIcon.Visible = true;
+            }
+        }
+
+        private void MenuShow_Click(object sender, EventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            this.notifyIcon.Visible = false;
+        }
+
+        private void MenuExit_Click(object sender, EventArgs e)
+        {
+            this.notifyIcon.Visible = false;
+            Environment.Exit(0);
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            this.notifyIcon.Visible = false;
+        }
+    }
+}
